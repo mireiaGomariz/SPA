497d0aba25e1cb39b9f90b05bb5377060d9e71e3
mireiaGomariz <mireiapgomariz@gmail.com>
2019-09-02 11:38:37 +0200

    Rutas usando links

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { HeroesService, Heroe } from '../../services/heroes.service';
 
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,16 +15,20 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService:HeroesService ) {
-
-
+  constructor( private _heroesService:HeroesService,
+               private router:Router
+             ) {
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
-    console.log( this.heroes );
+    // console.log( this.heroes );
   }
 
+  verHeroe( idx:number ) {
+    this.router.navigate( ['/heroe', idx])
+
+  }
 
 
 }
