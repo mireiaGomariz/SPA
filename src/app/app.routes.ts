617f316641d0bfd455d3e4c448a6b742eead23e1
mireiaGomariz <mireiapgomariz@gmail.com>
2019-09-26 11:33:14 +0200

    Añadir routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {HomeComponent} from './components/home/home.component';
 import {AboutComponent} from './components/about/about.component';
 import {HeroesComponent} from './components/heroes/heroes.component';
 import {HeroeComponent} from './components/heroe/heroe.component';
+import {BuscadorComponent} from './components/buscador/buscador.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
