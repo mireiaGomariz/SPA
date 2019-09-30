15c5d1ea976c551f48f12cd33d82dc8858cfe876
mireiaGomariz <mireiapgomariz@gmail.com>
2019-09-30 17:11:21 +0200

    heroes-tarjeta @input

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
-  styleUrls: ['./heroe-tarjeta.component.css']
 })
 export class HeroeTarjetaComponent implements OnInit {
+  
+  heroe:any = {};
 
   constructor() { }
 
