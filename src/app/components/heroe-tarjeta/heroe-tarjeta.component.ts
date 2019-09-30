91b70790d365ada92a1e0145d1760c90936870ba
mireiaGomariz <mireiapgomariz@gmail.com>
2019-09-30 17:40:09 +0200

    input hasta redireccion

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
 })
 export class HeroeTarjetaComponent implements OnInit {
-  
-  heroe:any = {};
+
+  @Input() heroe:any = {};
+  @Input() index:number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  verHeroe() {
+    console.log( this.index );
+  }
 }
